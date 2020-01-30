1d00cbc52471090d17cebcae08c04e0ed80b15f7
ykqin <30957970+ykqin@users.noreply.github.com>
2020-01-30 19:47:18 +0800

    Update frequencyConfig.py

--- frequencyConfig.py
+++ frequencyConfig.py
@@ -1,1 +1,10 @@
 
+
+# channel raster
+
+F_ref # RF reference frequecy
+N_ref # NR Absolute Radio Frequency Channel Number (NR-ARFCN) range:[0:2016666]
+deltaF_global # granularity of the global frequency raster
+
+
+F_ref = F_refoffs + deltaF_global * (N_ref - N_refoffs)
